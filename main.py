ca66c30195b3931b199546f76e15b62bbbfa0153
uEduardo2124 <156321223+uEduardo2124@users.noreply.github.com>
2024-01-23 11:53:36 -0300

    v2.0

---
+++ main.py
@@ -0,0 +1,152 @@
+import random
+
+while True:
+    interface = ['''            
+                --------
+                |      |
+                |
+                |
+                |
+                |
+                -''',
+                '''
+                --------
+                |      |
+                |      O
+                |
+                |
+                |
+                -''',
+                '''
+                --------
+                |      |
+                |      O
+                |      |
+                |
+                |
+                -''',
+                '''
+                --------
+                |      |
+                |      O
+                |     \|
+                |       
+                |
+                -''',
+                '''
+                --------
+                |      |
+                |      O
+                |     \|/
+                |       
+                |
+                -''',
+                '''
+                --------
+                |      |
+                |      O
+                |     \|/
+                |      |
+                |
+                -''',
+                '''
+                --------
+                |      |
+                |      O
+                |     \|/
+                |      |
+                |     /
+                -''']
+
+    def sorteio():
+        palavras_jogo = ['Homem de Ferro', 'Pixels', 'Transformers', 'Sonic', 'Vingadores Guerra Infinita', 'Tartarugas Ninja']
+        escolhido = random.choice(palavras_jogo)
+        return escolhido
+
+
+    class Features:
+        def __init__(self, palavra):
+            self.palavra = palavra
+            self.palavra_errada = []
+            self.palavra_certa = []
+
+
+        def intro(self):
+            print('-' * 42)
+            print('\033[32mBem vindo ao joigo da Forca\033[m')
+            print('_' * 42)
+            print('Adivinhe qual a palavra correta (Assunto: filmes da cultura pop)')
+            print('OBS:As primeiras letras de cada palavra serão maiúsculas(incluso caso hajam espaços no nome)')
+
+
+        def guess(self, letra):
+            if letra in self.palavra and letra not in self.palavra_certa:
+                self.palavra_certa.append(letra)
+            elif letra not in self.palavra and letra not in self.palavra_errada:
+                self.palavra_errada.append(letra)
+            else:
+                return False
+            return True
+
+
+        def game_its_over(self):
+            return self.player_won() or (len(self.palavra_errada) == 6)
+
+
+        def player_won(self):
+            if not '_' in self.esconder():
+                return True
+            return False
+
+
+        def esconder(self):
+            palavra_escondida = ''
+
+            for letra in self.palavra:
+                if letra not in self.palavra_certa:
+                    palavra_escondida += '_'
+                else:
+                    palavra_escondida += letra
+            return palavra_escondida
+
+
+        def status(self):
+            print(f'Forca: {interface[len(self.palavra_errada)]}')
+            print(f'Palavra: {self.esconder()}')
+            print('Letras erradas: ')
+            for letra in self.palavra_errada:
+                print(letra, end = ' ')
+            print()
+
+            print('Letras corretas: ')
+            for letra in self.palavra_certa:
+                print(letra, end = ' ')
+            print()
+
+
+    jogando = Features(sorteio())
+    jogando.intro()
+
+    while not jogando.game_its_over():
+        jogando.status()
+        try:
+            usuario = str(input('Digite uma letra: '))[0]
+        except:
+            print('\033[31mERRO, por favor tente novamante.\033[m')
+        else:
+            jogando.guess(usuario)
+
+    jogando.status()
+    if jogando.player_won():
+        print('\033[32mParabéns, você VENCEU!\033[m')
+    else:
+        print('\033[31mGAME OVER, você perdeu!\033[m')
+
+    try:
+        continuar = str(input('Deseja continuar [S/N]? ')).upper().split()[0]
+    except:
+        print('\033[31mERRO, por favor digite novamente\033[m')
+    else:
+        if continuar == 'N':
+            break
+print('\033[35mMuito obrigado por jogar, volte sempre ;)\033[m')
